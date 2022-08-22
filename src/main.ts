bf28c31b5942eb8172717464a633980a0e9c7a36
ukyi <devcat.js@gmail.com>
2022-08-22 12:06:16 +0900

    🔊 server running Logger 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = 3000;
+  await app.listen(port);
+
+  Logger.log(`🚀 Server running on http://localhost:${port}`, 'Server');
 }
+
 bootstrap();
