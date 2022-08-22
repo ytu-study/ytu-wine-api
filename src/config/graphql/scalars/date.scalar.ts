cd2b95bdb4a10df7b3b9679898ac8efe44fbf7b3
ukyi <devcat.js@gmail.com>
2022-08-22 12:06:46 +0900

    🔧 DateScalar 추가

---
+++ src/config/graphql/scalars/date.scalar.ts
@@ -0,0 +1,20 @@
+import { CustomScalar, Scalar } from '@nestjs/graphql';
+import { Kind, ValueNode } from 'graphql';
+
+@Scalar('Date', () => Date)
+export class DateScalar implements CustomScalar<number, Date> {
+  public description = 'Date custom scalar type';
+
+  public parseValue(value: number): Date {
+    return new Date(value); // value from the client
+  }
+
+  public serialize(value: Date): number {
+    return value.getTime(); // value sent to the client
+  }
+
+  public parseLiteral(ast: ValueNode): Date | null {
+    if (ast.kind === Kind.INT) return new Date(ast.value);
+    return null;
+  }
+}
