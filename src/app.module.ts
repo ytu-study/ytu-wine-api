54c917641fa22668d2a55deea48dd4113499d06c
ukyi <devcat.js@gmail.com>
2022-08-22 12:07:36 +0900

    ✨ ConfigModule, GraphQLModule, AppResolver 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { GraphqlService } from '@/config/graphql';
 import { AppController } from './app.controller';
+import { AppResolver } from './app.resolver';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({ useClass: GraphqlService, driver: ApolloDriver }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AppResolver],
 })
-export class AppModule {}
+export class AppModule {
+}
