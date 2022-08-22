6c55e0f7aef892adfdf8895a786d9b2dc442dcd6
ukyi <devcat.js@gmail.com>
2022-08-22 12:06:39 +0900

    🔧 graphql config 추가

---
+++ src/config/graphql/index.ts
@@ -0,0 +1,26 @@
+import { ApolloDriverConfig } from '@nestjs/apollo';
+import { Injectable } from '@nestjs/common';
+import { GqlOptionsFactory } from '@nestjs/graphql';
+import { GraphQLError, GraphQLFormattedError } from 'graphql';
+import { ObjectLiteral } from 'typeorm';
+
+@Injectable()
+export class GraphqlService implements GqlOptionsFactory {
+  private endpoint = '/api/graphql';
+
+  createGqlOptions(): ApolloDriverConfig {
+    return {
+      path: this.endpoint,
+      autoSchemaFile: true,
+      playground: { endpoint: this.endpoint },
+      cors: true,
+      introspection: true,
+      formatError: (error: GraphQLError): GraphQLFormattedError & ObjectLiteral => ({
+        message: error.message,
+        code: error.extensions?.code,
+        locations: error.locations,
+        path: error.path,
+      }),
+    };
+  }
+}
