37ef53b2f004adc0d3bde24f99d77022e1724924
ukyi <devcat.js@gmail.com>
2022-08-24 01:59:13 +0900

    🔥 expressApp 제거

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,13 @@
-import { INestApplication, Logger } from '@nestjs/common';
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import { Express } from 'express';
 import { AppModule } from '@/app.module';
 
-export const createServer = async (expressApp?: Express): Promise<INestApplication> => {
-  if (expressApp) return NestFactory.create(AppModule, new ExpressAdapter(expressApp), { cors: true });
-
-  const app = await NestFactory.create(AppModule, expressApp ? new ExpressAdapter(expressApp) : undefined, { cors: true });
+export const createServer = async (): Promise<void> => {
+  const app = await NestFactory.create(AppModule, { cors: true });
   const port = +process.env.PORT;
   await app.listen(port);
 
   Logger.log(`🚀 Server running on http://localhost:${port}`, 'Server');
-  return app;
 };
 
 createServer();
