6a6f6051e931716e212c80dc1112e96c1d5daf96
ukyi <devcat.js@gmail.com>
2022-08-24 00:21:33 +0900

    ✨ expressApp있을때 수정

--- src/main.ts
+++ src/main.ts
@@ -5,18 +5,13 @@ import { Express } from 'express';
 import { AppModule } from '@/app.module';
 
 export const createServer = async (expressApp?: Express): Promise<INestApplication> => {
-  let app: INestApplication;
-  if (expressApp) {
-    app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp), { cors: true });
-    return app.init();
-  }
-  app = await NestFactory.create(AppModule, expressApp ? new ExpressAdapter(expressApp) : undefined, { cors: true });
+  if (expressApp) return NestFactory.create(AppModule, new ExpressAdapter(expressApp), { cors: true });
 
+  const app = await NestFactory.create(AppModule, expressApp ? new ExpressAdapter(expressApp) : undefined, { cors: true });
   const port = +process.env.PORT;
   await app.listen(port);
 
   Logger.log(`🚀 Server running on http://localhost:${port}`, 'Server');
-
   return app;
 };
 
