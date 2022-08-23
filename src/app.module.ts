82629b2c1c9a753d7d8114fb04efada61cada8fb
ukyi <devcat.js@gmail.com>
2022-08-23 19:37:58 +0900

    ✨ TypeOrmModule, WineModule 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,21 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from '@/app.controller';
+import { AppResolver } from '@/app.resolver';
+import { AppService } from '@/app.service';
+import { configOptions } from '@/config/env';
 import { GraphqlService } from '@/config/graphql';
-import { AppController } from './app.controller';
-import { AppResolver } from './app.resolver';
-import { AppService } from './app.service';
+import { TypeormService } from '@/config/typeorm';
+import { WineModule } from './wine/wine.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot(configOptions),
     GraphQLModule.forRootAsync<ApolloDriverConfig>({ useClass: GraphqlService, driver: ApolloDriver }),
+    TypeOrmModule.forRootAsync({ useClass: TypeormService }),
+    WineModule,
   ],
   controllers: [AppController],
   providers: [AppService, AppResolver],
