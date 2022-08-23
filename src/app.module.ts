3db66b51443afbc31ac48670b02fa35e9af5eee8
ukyi <devcat.js@gmail.com>
2022-08-23 22:50:29 +0900

    ✨ ValidationPipe, HttpExceptionFilter, LoggingInterceptor, TimeoutInterceptor 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,18 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from '@/app.controller';
 import { AppResolver } from '@/app.resolver';
 import { AppService } from '@/app.service';
+import { HttpExceptionFilter } from '@/common/filters';
+import { LoggingInterceptor, TimeoutInterceptor } from '@/common/interceptors';
 import { configOptions } from '@/config/env';
 import { GraphqlService } from '@/config/graphql';
 import { TypeormService } from '@/config/typeorm';
-import { WineModule } from './wine/wine.module';
+import { WineModule } from '@/wine/wine.module';
 
 @Module({
   imports: [
@@ -18,8 +21,17 @@ import { WineModule } from './wine/wine.module';
     TypeOrmModule.forRootAsync({ useClass: TypeormService }),
     WineModule,
   ],
-  controllers: [AppController],
-  providers: [AppService, AppResolver],
+  controllers: [
+    AppController,
+  ],
+  providers: [
+    { provide: APP_PIPE, useClass: ValidationPipe },
+    { provide: APP_FILTER, useClass: HttpExceptionFilter },
+    { provide: APP_INTERCEPTOR, useClass: LoggingInterceptor },
+    { provide: APP_INTERCEPTOR, useClass: TimeoutInterceptor },
+    AppService,
+    AppResolver,
+  ],
 })
 export class AppModule {
 }
