6059388a9402095e4cbeaca5d3bc698e61f3adf8
ukyi <devcat.js@gmail.com>
2022-08-24 00:23:33 +0900

    ✨ Args 수정

--- src/wine/wine.service.ts
+++ src/wine/wine.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { MongoRepository } from 'typeorm';
-import { Pagination } from '@/wine/model/pagination.args';
 import { Wine } from '@/wine/model/wine.entity';
 
 @Injectable()
@@ -9,8 +8,7 @@ export class WineService {
   constructor(@InjectRepository(Wine) private readonly wineRepository: MongoRepository<Wine>) {
   }
 
-  getWines(pagination?: Pagination): Promise<Wine[]> {
-    const { page = 1, display = 10 } = pagination ?? {};
+  getWines(page: number, display: number): Promise<Wine[]> {
     return this.wineRepository.find({
       skip: (page - 1) * display,
       take: display,
