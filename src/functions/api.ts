8d8d9160c1d0afa6ba01b286c1193a8c5762872a
ukyi <devcat.js@gmail.com>
2022-08-24 01:37:04 +0900

    🔧 setGlobalPrefix 제거

--- src/functions/api.ts
+++ src/functions/api.ts
@@ -1,19 +1,11 @@
-import { INestApplication } from '@nestjs/common';
 import * as express from 'express';
-import { readdirSync } from 'fs';
-import { join } from 'path';
 import * as serverless from 'serverless-http';
 import { createServer } from '@/main';
-
-const functionsName = readdirSync(join(__dirname)).find((name) => name.includes('js')) ?? 'api';
-const GLOBAL_PREFIX = functionsName.replace(/\.js/, '');
 
 const expressApp = express();
 
 export const handler = async (event: unknown, context: unknown): Promise<unknown> => {
-  const app: INestApplication = await createServer(expressApp);
-
-  await app.setGlobalPrefix(`.netlify/functions/${GLOBAL_PREFIX}`).init();
+  await createServer(expressApp).then(app => app.init());
 
   const appHandler = serverless(expressApp);
   return appHandler(event, context);
