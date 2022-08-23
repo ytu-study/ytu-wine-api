9a127d3f0b18bdcc052b74b0873efeb249e31129
ukyi <devcat.js@gmail.com>
2022-08-23 23:41:29 +0900

    ✨ autoSchemaFile 수정

--- src/config/graphql/index.ts
+++ src/config/graphql/index.ts
@@ -2,18 +2,16 @@ import { ApolloDriverConfig } from '@nestjs/apollo';
 import { Injectable } from '@nestjs/common';
 import { GqlOptionsFactory } from '@nestjs/graphql';
 import { GraphQLError, GraphQLFormattedError } from 'graphql';
-import { join } from 'path';
 import { ObjectLiteral } from 'typeorm';
 
 @Injectable()
 export class GraphqlService implements GqlOptionsFactory {
   private endpoint = '/api/graphql';
-  private isDev = process.env.NODE_ENV === 'development';
 
   createGqlOptions(): ApolloDriverConfig {
     return {
       path: this.endpoint,
-      autoSchemaFile: this.isDev ? join(process.cwd(), 'src/schema.gql') : true,
+      autoSchemaFile: true,
       playground: { endpoint: this.endpoint },
       cors: true,
       introspection: true,
