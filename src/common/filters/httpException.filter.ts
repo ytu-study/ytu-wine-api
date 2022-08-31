fc6b30594edf99061f694a65660b365fd0d293e4
ukyi <devcat.js@gmail.com>
2022-09-01 00:56:01 +0900

    ✨ graphql HttpExceptionFilter 추가

--- src/common/filters/httpException.filter.ts
+++ src/common/filters/httpException.filter.ts
@@ -1,19 +1,9 @@
-import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from '@nestjs/common';
-import { Request, Response } from 'express';
+import { Catch, HttpException } from '@nestjs/common';
+import { GqlExceptionFilter } from '@nestjs/graphql';
 
 @Catch(HttpException)
-export class HttpExceptionFilter implements ExceptionFilter {
-  public catch(exception: HttpException, host: ArgumentsHost): void {
-    const ctx = host.switchToHttp();
-    const response = ctx.getResponse<Response>();
-    const request = ctx.getRequest<Request>();
-    const status = exception.getStatus();
-
-    response.status(status).json({
-      statusCode: status,
-      message: exception.message,
-      timestamp: new Date().toISOString(),
-      path: request.url,
-    });
+export class HttpExceptionFilter implements GqlExceptionFilter {
+  catch(exception: HttpException): HttpException {
+    return exception;
   }
 }
