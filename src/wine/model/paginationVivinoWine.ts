c97bda03377034db02ebf2f147a368db965f6513
ukyi <devcat.js@gmail.com>
2022-09-15 13:28:18 +0900

    ✨ PaginationVivinoWine ObjectType 추가

---
+++ src/wine/model/paginationVivinoWine.ts
@@ -0,0 +1,28 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+import { Expose, plainToClass } from 'class-transformer';
+import { VivinoWine } from '@/wine/model/vivinoWine.entity';
+import { PaginationItems } from '@/types/PaginationItems';
+
+@ObjectType({
+  implements: () => [PaginationItems],
+})
+export class PaginationVivinoWine implements PaginationItems<VivinoWine[]> {
+  @Expose()
+  page: number;
+
+  @Expose()
+  display: number;
+
+  @Expose()
+  totalCount: number;
+
+  @Expose()
+  @Field(() => [VivinoWine])
+  items: VivinoWine[];
+
+  constructor(pagination: PaginationVivinoWine) {
+    if (!pagination?.items) return;
+
+    Object.assign(this, plainToClass(PaginationVivinoWine, pagination, { excludeExtraneousValues: true }));
+  }
+}
