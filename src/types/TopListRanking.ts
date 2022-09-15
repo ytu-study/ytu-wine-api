fae8f92ccf7c75e34563a228484df0449d96d0a0
ukyi <devcat.js@gmail.com>
2022-09-15 12:14:19 +0900

    ✨ TopListRanking ObjectType 추가

---
+++ src/types/TopListRanking.ts
@@ -0,0 +1,17 @@
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { TopList } from '@/types/TopList';
+
+@ObjectType()
+export class TopListRanking {
+  @Field(() => Int, { description: '' })
+  rank: number;
+
+  @Field(() => Int, { description: '' })
+  previousRank: number;
+
+  @Field({ nullable: true, description: '' })
+  description: string | null;
+
+  @Field(() => TopList, { description: '' })
+  topList: TopList;
+}
