4acd1820332dbbcd1464f34a6ecfa0c5f28d5d6b
ukyi <devcat.js@gmail.com>
2022-09-15 12:12:24 +0900

    ✨ wineCountryRepository, wineFoodRepository, wineGrapeRepository, wineTypeRepository 추가

--- src/wine/wine.service.ts
+++ src/wine/wine.service.ts
@@ -1,12 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { MongoRepository } from 'typeorm';
+import { WineType } from '@/wine/model/wineType.entity';
+import { WineCountry } from '@/wine/model/wineCountry.entity';
+import { WineFood } from '@/wine/model/wineFood.entity';
+import { WineGrape } from '@/wine/model/wineGrape.entity';
 import { PaginationArgs } from '@/args/pagination.args';
+import { VivinoWine } from '@/wine/model/vivinoWine.entity';
 import { Wine } from '@/wine/model/wine.entity';
 
 @Injectable()
 export class WineService {
-  constructor(@InjectRepository(Wine) private readonly wineRepository: MongoRepository<Wine>) {
+  constructor(
+    @InjectRepository(Wine) private readonly wineRepository: MongoRepository<Wine>,
+    @InjectRepository(VivinoWine) private readonly vivinoWineRepository: MongoRepository<VivinoWine>,
+    @InjectRepository(WineCountry) private readonly wineCountryRepository: MongoRepository<WineCountry>,
+    @InjectRepository(WineFood) private readonly wineFoodRepository: MongoRepository<WineFood>,
+    @InjectRepository(WineGrape) private readonly wineGrapeRepository: MongoRepository<WineGrape>,
+    @InjectRepository(WineType) private readonly wineTypeRepository: MongoRepository<WineType>,
+  ) {
   }
 
   getWines({ page, display }: PaginationArgs): Promise<Wine[]> {
@@ -22,5 +34,12 @@ export class WineService {
 
   getWineIds(): Promise<string[]> {
     return this.wineRepository.find({ select: ['_id'] }).then(wines => wines.map(wine => wine._id));
+  }
+
+  getVivinoWines({ page, display }: PaginationArgs):Promise<VivinoWine[]> {
+    return this.vivinoWineRepository.find({
+      skip: (page - 1) * display,
+      take: display,
+    });
   }
 }
