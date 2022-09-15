8f21d9907cdbd09248ca53e59c78f1e0029bd673
ukyi <devcat.js@gmail.com>
2022-09-15 13:30:53 +0900

    ✨ PaginationWineFood ObjectType 추가

---
+++ src/wine/model/paginationWineFood.ts
@@ -0,0 +1,28 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+import { Expose, plainToClass } from 'class-transformer';
+import { WineFood } from '@/wine/model/wineFood.entity';
+import { PaginationItems } from '@/types/PaginationItems';
+
+@ObjectType({
+  implements: () => [PaginationItems],
+})
+export class PaginationWineFood implements PaginationItems<WineFood[]> {
+  @Expose()
+  page: number;
+
+  @Expose()
+  display: number;
+
+  @Expose()
+  totalCount: number;
+
+  @Expose()
+  @Field(() => [WineFood])
+  items: WineFood[];
+
+  constructor(pagination: PaginationWineFood) {
+    if (!pagination?.items) return;
+
+    Object.assign(this, plainToClass(PaginationWineFood, pagination, { excludeExtraneousValues: true }));
+  }
+}
