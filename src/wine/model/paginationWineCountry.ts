2ca1d9db38bd5e08d65b03d1100e036a20582b76
ukyi <devcat.js@gmail.com>
2022-09-15 13:30:48 +0900

    ✨ PaginationWineCountry ObjectType 추가

---
+++ src/wine/model/paginationWineCountry.ts
@@ -0,0 +1,27 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+import { Expose, plainToClass } from 'class-transformer';
+import { WineCountry } from '@/wine/model/wineCountry.entity';
+import { PaginationItems } from '@/types/PaginationItems';
+
+@ObjectType({
+  implements: () => [PaginationItems],
+})
+export class PaginationWineCountry implements PaginationItems<WineCountry[]> {
+  @Expose()
+  page: number;
+
+  @Expose()
+  display: number;
+
+  @Expose()
+  totalCount: number;
+
+  @Field(() => [WineCountry])
+  items: WineCountry[];
+
+  constructor(pagination: PaginationWineCountry) {
+    if (!pagination?.items) return;
+
+    Object.assign(this, plainToClass(PaginationWineCountry, pagination, { excludeExtraneousValues: true }));
+  }
+}
