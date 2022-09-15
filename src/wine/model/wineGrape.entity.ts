c33b9874eee8176e88ed0532910712d14ddcd891
ukyi <devcat.js@gmail.com>
2022-09-15 12:07:34 +0900

    ✨ WineGrape entity 추가

---
+++ src/wine/model/wineGrape.entity.ts
@@ -0,0 +1,43 @@
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { Expose, plainToClass } from 'class-transformer';
+import { Column, Entity, ObjectIdColumn } from 'typeorm';
+
+@Entity()
+@ObjectType({ description: '와인 포도 정보' })
+export class WineGrape {
+  @Expose()
+  @ObjectIdColumn()
+  @Field({ description: 'objectId' })
+  _id?: string;
+
+  @Expose()
+  @Column()
+  @Field(() => Int, { description: '' })
+  id: number;
+
+  @Expose()
+  @Column()
+  @Field({ description: '' })
+  name: string;
+
+  @Expose()
+  @Column()
+  @Field({ description: '' })
+  seoName: string;
+
+  @Expose()
+  @Column()
+  @Field(() => Int, { description: '' })
+  hasDetailedInfo: boolean;
+
+  @Expose()
+  @Column()
+  @Field(() => Int, { description: '' })
+  winesCount: number;
+
+  constructor(wineGrape: Partial<WineGrape>) {
+    if (!wineGrape?.name) return;
+
+    Object.assign(this, plainToClass(WineGrape, wineGrape, { excludeExtraneousValues: true }));
+  }
+}
