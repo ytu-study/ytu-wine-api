afcfe5253d6b4e7b5dec3cf718e6e04a82aa4c5e
ukyi <devcat.js@gmail.com>
2022-09-15 11:47:29 +0900

    ✨ WineFood entity 추가

---
+++ src/wine/model/wineFood.entity.ts
@@ -0,0 +1,34 @@
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { Expose, plainToClass } from 'class-transformer';
+import { Column, Entity, ObjectIdColumn } from 'typeorm';
+import { FoodBackgroundImage } from '@/types/FoodBackgroundImage';
+
+@Entity()
+@ObjectType({ description: '와인과 어울리는 음식 정보' })
+export class WineFood {
+  @Expose()
+  @ObjectIdColumn()
+  @Field({ description: 'objectId' })
+  _id?: string;
+
+  @Expose()
+  @Column()
+  @Field(() => Int, { description: '' })
+  id: number;
+
+  @Expose()
+  @Column()
+  @Field({ description: '' })
+  name: string;
+
+  @Expose()
+  @Column()
+  @Field(() => FoodBackgroundImage, { description: '' })
+  backgroundImage: FoodBackgroundImage;
+
+  constructor(wineFood: Partial<WineFood>) {
+    if (!wineFood?.name) return;
+
+    Object.assign(this, plainToClass(WineFood, wineFood, { excludeExtraneousValues: true }));
+  }
+}
