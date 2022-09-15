9ba09d8d474d9244ddba74224e6fa29b45c2cc58
ukyi <devcat.js@gmail.com>
2022-09-15 12:07:39 +0900

    ✨ WineType entity 추가

---
+++ src/wine/model/wineType.entity.ts
@@ -0,0 +1,33 @@
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { Expose, plainToClass } from 'class-transformer';
+import { Column, Entity, ObjectIdColumn } from 'typeorm';
+
+@Entity()
+@ObjectType({ description: '와인 타입 정보' })
+export class WineType {
+  @Expose()
+  @ObjectIdColumn()
+  @Field({ description: 'objectId' })
+  _id?: string;
+
+  @Expose()
+  @Column()
+  @Field(() => Int, { description: '' })
+  id: number;
+
+  @Expose()
+  @Column()
+  @Field({ description: '' })
+  name: string;
+
+  @Expose()
+  @Column()
+  @Field(() => Int, { description: '' })
+  winesCount: number;
+
+  constructor(wineType: Partial<WineType>) {
+    if (!wineType?.name) return;
+
+    Object.assign(this, plainToClass(WineType, wineType, { excludeExtraneousValues: true }));
+  }
+}
