633821c71318f6a270906016dd9c0535c2918cf2
ukyi <devcat.js@gmail.com>
2022-09-18 15:33:41 +0900

    ♻️ PaginationWineGrape으로 수정

--- src/wine/model/paginationWineGrape.ts
+++ src/wine/model/paginationWineGrape.ts
@@ -1,28 +1,7 @@
-import { Field, ObjectType } from '@nestjs/graphql';
-import { Expose, plainToClass } from 'class-transformer';
-import { PaginationItems } from '@/types/paginationItems';
+import { ObjectType } from '@nestjs/graphql';
+import { Paginated } from '@/utils/paginated';
 import { WineGrape } from '@/wine/model/wineGrape.entity';
 
-@ObjectType({
-  implements: () => [PaginationItems],
-})
-export class PaginationWineGrape implements PaginationItems<WineGrape[]> {
-  @Expose()
-  page: number;
-
-  @Expose()
-  display: number;
-
-  @Expose()
-  totalCount: number;
-
-  @Expose()
-  @Field(() => [WineGrape], { nullable: 'items' })
-  items: WineGrape[];
-
-  constructor(pagination: PaginationWineGrape) {
-    if (!pagination?.items) return;
-
-    Object.assign(this, plainToClass(PaginationWineGrape, pagination, { excludeExtraneousValues: true }));
-  }
+@ObjectType()
+export class PaginationWineGrape extends Paginated(WineGrape) {
 }
