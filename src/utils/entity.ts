1950dc6d80ab858bd79d1536c7549c1a5ef388be
ukyi <devcat.js@gmail.com>
2022-09-18 15:29:40 +0900

    ✨ BaseEntity util 추가

---
+++ src/utils/entity.ts
@@ -0,0 +1,33 @@
+import { Type } from '@nestjs/common';
+import { Field, ID, ObjectType } from '@nestjs/graphql';
+import { Exclude, plainToInstance } from 'class-transformer';
+import isEmpty from 'lodash/isEmpty';
+import { ObjectIdColumn } from 'typeorm';
+
+interface IBaseEntity {
+  _id?: string;
+}
+
+interface IEntity extends Type<IBaseEntity> {
+  description: string;
+}
+
+export function BaseEntity(description: string): IEntity {
+  @ObjectType({ isAbstract: true })
+  class BaseEntity<T> {
+    static description = description;
+
+    @Exclude()
+    @ObjectIdColumn()
+    @Field(() => ID, { description: 'objectId' })
+    _id?: string;
+
+    constructor(classRef: Type<T>, entity: Partial<T>) {
+      if (isEmpty(entity)) return;
+
+      Object.assign(this, plainToInstance(classRef, entity));
+    }
+  }
+
+  return BaseEntity as IEntity;
+}
