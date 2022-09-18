605db5c48102e0df81e57a2b55611f033fe72180
ukyi <devcat.js@gmail.com>
2022-09-18 15:29:34 +0900

    ✨ Paginated util 추가

---
+++ src/utils/paginated.ts
@@ -0,0 +1,35 @@
+import { Type } from '@nestjs/common';
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { Min } from 'class-validator';
+
+interface ClassRef<T> extends Type<T> {
+  description: string;
+}
+
+export interface IPaginatedType<T> {
+  page: number;
+  display: number;
+  totalCount: number;
+  items: T[];
+}
+
+export function Paginated<T>(classRef: ClassRef<T>): Type<IPaginatedType<T>> {
+  @ObjectType({ isAbstract: true })
+  abstract class PaginatedType implements IPaginatedType<T> {
+    @Min(1)
+    @Field(() => Int, { description: '현재 페이지' })
+    page: number;
+
+    @Min(1)
+    @Field(() => Int, { description: '현재 페이지당 보여줄 목록 수' })
+    display: number;
+
+    @Field(() => Int, { description: '총 개수' })
+    totalCount: number;
+
+    @Field(() => [classRef], { nullable: 'items', description: classRef.description })
+    items: T[];
+  }
+
+  return PaginatedType as Type<IPaginatedType<T>>;
+}
