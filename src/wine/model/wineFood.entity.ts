8feb93c1cd20a890bc7a5a6e1968fdaca5c7425a
ukyi <devcat.js@gmail.com>
2022-09-25 00:10:11 +0900

    ✨ OmitType id 추가

--- src/wine/model/wineFood.entity.ts
+++ src/wine/model/wineFood.entity.ts
@@ -1,4 +1,4 @@
-import { ObjectType } from '@nestjs/graphql';
+import { Field, Int, ObjectType, OmitType } from '@nestjs/graphql';
 import { Column, Entity } from 'typeorm';
 import { FoodBackgroundImage } from '@/types/foodBackgroundImage';
 import { WineFood } from '@/types/wineFood';
@@ -6,9 +6,10 @@ import { BaseEntity } from '@/utils/entity';
 
 @Entity({ name: 'wine_food' })
 @ObjectType({ description: '와인과 어울리는 음식' })
-export class WineFoodEntity extends BaseEntity(WineFood) implements WineFood {
+export class WineFoodEntity extends BaseEntity(OmitType(WineFood, ['id'])) implements Omit<WineFood, 'id'> {
   @Column({ name: 'id' })
-  foodId: number;
+  @Field(() => Int, { description: '음식 ID' })
+  foodId: WineFood['id'];
 
   @Column()
   name: string;
--- src/wine/model/wineGrape.entity.ts
+++ src/wine/model/wineGrape.entity.ts
@@ -1,12 +1,13 @@
-import { ObjectType } from '@nestjs/graphql';
+import { Field, Int, ObjectType, OmitType } from '@nestjs/graphql';
 import { Column, Entity } from 'typeorm';
 import { WineGrape } from '@/types/wineGrape';
 import { BaseEntity } from '@/utils/entity';
 
 @Entity({ name: 'wine_grape' })
 @ObjectType({ description: '와인 포도 품종' })
-export class WineGrapeEntity extends BaseEntity(WineGrape) implements WineGrape {
+export class WineGrapeEntity extends BaseEntity(OmitType(WineGrape, ['id'])) implements Omit<WineGrape, 'id'> {
   @Column({ name: 'id' })
+  @Field(() => Int, { description: '포도 ID' })
   grapeId: number;
 
   @Column()
