1123f7519acd9c1ceb054232090f89ac9d51c6a1
ukyi <devcat.js@gmail.com>
2022-09-24 22:39:33 +0900

    ✨ BaseEntity WineGrape 추가

--- src/wine/model/wineGrape.entity.ts
+++ src/wine/model/wineGrape.entity.ts
@@ -1,33 +1,15 @@
-import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { ObjectType } from '@nestjs/graphql';
 import { Column, Entity } from 'typeorm';
+import { WineGrape } from '@/types/wineGrape';
 import { BaseEntity } from '@/utils/entity';
 
-const description = '와인 포도 품종';
+@Entity({ name: 'wine_grape' })
+@ObjectType({ description: '와인 포도 품종' })
+export class WineGrapeEntity extends BaseEntity(WineGrape) {
+  @Column({ name: 'id' })
+  grapeId: number;
 
-@Entity()
-@ObjectType({ description })
-export class WineGrape extends BaseEntity(description) {
-  @Column()
-  @Field(() => Int, { description: '' })
-  id: number;
-
-  @Column()
-  @Field({ description: '' })
-  name: string;
-
-  @Column()
-  @Field({ description: '' })
-  seoName: string;
-
-  @Column()
-  @Field(() => Int, { description: '' })
-  hasDetailedInfo: boolean;
-
-  @Column()
-  @Field(() => Int, { description: '' })
-  winesCount: number;
-
-  constructor(data: WineGrape) {
-    super(WineGrape, data);
+  constructor(data: WineGrapeEntity) {
+    super(WineGrapeEntity, data);
   }
 }
