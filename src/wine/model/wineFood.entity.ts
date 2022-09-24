08f546ba43fa21758dd3e95027ab00bbb8f1e8d6
ukyi <devcat.js@gmail.com>
2022-09-24 22:39:28 +0900

    ✨ BaseEntity WineFood 추가

--- src/wine/model/wineFood.entity.ts
+++ src/wine/model/wineFood.entity.ts
@@ -1,26 +1,15 @@
-import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { ObjectType } from '@nestjs/graphql';
 import { Column, Entity } from 'typeorm';
-import { FoodBackgroundImage } from '@/types/foodBackgroundImage';
+import { WineFood } from '@/types/wineFood';
 import { BaseEntity } from '@/utils/entity';
 
-const description = '와인과 어울리는 음식';
+@Entity({ name: 'wine_food' })
+@ObjectType({ description: '와인과 어울리는 음식' })
+export class WineFoodEntity extends BaseEntity(WineFood) {
+  @Column({ name: 'id' })
+  foodId: number;
 
-@Entity()
-@ObjectType({ description })
-export class WineFood extends BaseEntity(description) {
-  @Column()
-  @Field(() => Int, { description: '' })
-  id: number;
-
-  @Column()
-  @Field({ description: '' })
-  name: string;
-
-  @Column()
-  @Field(() => FoodBackgroundImage, { description: '' })
-  backgroundImage: FoodBackgroundImage;
-
-  constructor(wineFood: Partial<WineFood>) {
-    super(WineFood, wineFood);
+  constructor(wineFood: Partial<WineFoodEntity>) {
+    super(WineFoodEntity, wineFood);
   }
 }
