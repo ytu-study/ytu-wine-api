6a8682b81fc6eb005096b6a22bc594f73f342c01
ukyi <devcat.js@gmail.com>
2022-09-24 22:37:13 +0900

    🏷️ WineFood ObjectType 추가

---
+++ src/types/wineFood.ts
@@ -0,0 +1,14 @@
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { FoodBackgroundImage } from '@/types/foodBackgroundImage';
+
+@ObjectType({ description: '와인과 어울리는 음식' })
+export class WineFood {
+  @Field(() => Int, { description: '음식 ID' })
+  foodId: number;
+
+  @Field({ description: '음식 이름' })
+  name: string;
+
+  @Field(() => FoodBackgroundImage, { description: '음식 이미지' })
+  backgroundImage: FoodBackgroundImage;
+}
