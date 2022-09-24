153722e047e8ae8fc00740d60c2f1470c9d488d7
ukyi <devcat.js@gmail.com>
2022-09-24 22:37:17 +0900

    🏷️ WineGrape ObjectType 추가

---
+++ src/types/wineGrape.ts
@@ -0,0 +1,19 @@
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+
+@ObjectType({ description: '와인 포도 품종' })
+export class WineGrape {
+  @Field(() => Int, { description: '포도 ID' })
+  grapeId: number;
+
+  @Field({ description: '포도 이름' })
+  name: string;
+
+  @Field({ description: '포도 이름(kebab-case)' })
+  seoName: string;
+
+  @Field(() => Int, { deprecationReason: 'deprecated' })
+  hasDetailedInfo: boolean;
+
+  @Field(() => Int, { description: '포도 조회수' })
+  winesCount: number;
+}
