77ce6c3dc7bd13e39df50f3cf4b62ec54b61ba6a
ukyi <devcat.js@gmail.com>
2022-09-24 22:39:08 +0900

    ✨ BaseEntity WineCountry 추가

--- src/wine/model/wineCountry.entity.ts
+++ src/wine/model/wineCountry.entity.ts
@@ -1,55 +1,12 @@
-import { Field, Int, ObjectType } from '@nestjs/graphql';
-import { Column, Entity } from 'typeorm';
-import { Currency } from '@/types/currency';
+import { ObjectType } from '@nestjs/graphql';
+import { Entity } from 'typeorm';
+import { WineCountry } from '@/types/wineCountry';
 import { BaseEntity } from '@/utils/entity';
-import { WineGrape } from './wineGrape.entity';
 
-const description = '와인 국가';
-
-@Entity()
-@ObjectType({ description })
-export class WineCountry extends BaseEntity(description) {
-  @Column()
-  @Field({ description: '' })
-  code: string;
-
-  @Column()
-  @Field({ description: '' })
-  name: string;
-
-  @Column()
-  @Field({ description: '' })
-  nativeName: string;
-
-  @Column()
-  @Field({ description: '' })
-  seoName: string;
-
-  @Column()
-  @Field(() => Currency, { description: '' })
-  currency: Currency;
-
-  @Column()
-  @Field(() => Int, { description: '' })
-  regionsCount: number;
-
-  @Column()
-  @Field(() => Int, { description: '' })
-  usersCount: number;
-
-  @Column()
-  @Field(() => Int, { description: '' })
-  winesCount: number;
-
-  @Column()
-  @Field(() => Int, { description: '' })
-  wineriesCount: number;
-
-  @Column()
-  @Field(() => [WineGrape], { description: '' })
-  mostUsedGrapes: WineGrape[];
-
-  constructor(wineCountry: Partial<WineCountry>) {
-    super(WineCountry, wineCountry);
+@Entity({ name: 'wine_country' })
+@ObjectType({ description: '와인 국가' })
+export class WineCountryEntity extends BaseEntity(WineCountry) {
+  constructor(wineCountry: Partial<WineCountryEntity>) {
+    super(WineCountryEntity, wineCountry);
   }
 }
